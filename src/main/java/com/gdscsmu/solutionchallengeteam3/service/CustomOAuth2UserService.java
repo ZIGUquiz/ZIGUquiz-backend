71aedbc33becda1e7972b710a4aa369ae40be6cf
Sim-GyuBin <skb0606@gmail.com>
2024-02-07 17:50:43 +0900

    Feat: 로그인 uri 및 주석 수정

--- src/main/java/com/gdscsmu/solutionchallengeteam3/service/CustomOAuth2UserService.java
+++ src/main/java/com/gdscsmu/solutionchallengeteam3/service/CustomOAuth2UserService.java
@@ -1,11 +1,14 @@
 package com.gdscsmu.solutionchallengeteam3.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gdscsmu.solutionchallengeteam3.domain.auth.OAuthAttributes;
 import com.gdscsmu.solutionchallengeteam3.domain.auth.SessionUser;
 import com.gdscsmu.solutionchallengeteam3.domain.user.User;
 import com.gdscsmu.solutionchallengeteam3.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -14,19 +17,20 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collections;
 
 @RequiredArgsConstructor
-@Service
+@Service @Slf4j
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
     private final HttpSession httpSession;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
 
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
@@ -38,6 +42,8 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         User user = saveOrUpdate(attributes);
 
         httpSession.setAttribute("user", new SessionUser(user));    // 세션에 저장
+//        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+//        System.out.println(oAuth2User.getAttributes());
 
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())), attributes.getAttributes(), attributes.getNameAttributeKey());
     }
