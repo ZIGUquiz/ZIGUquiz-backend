e4c66236a1177bc793da2cf8cb3adbbf2e483ded
Sim-GyuBin <skb0606@gmail.com>
2024-02-08 00:58:02 +0900

    Feat: 세션 확인 폼 작성

--- src/main/java/com/gdscsmu/solutionchallengeteam3/controller/LoginController.java
+++ src/main/java/com/gdscsmu/solutionchallengeteam3/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.gdscsmu.solutionchallengeteam3.controller;
 
+import com.gdscsmu.solutionchallengeteam3.domain.auth.SessionUser;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,8 +12,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LoginController {
 
     @GetMapping("/login")
-    public String home() {
+    public String login() {
         return "loginForm";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session, HttpServletRequest request) {
+        session = request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/";
     }
 
     @GetMapping("/private")
@@ -20,4 +34,14 @@ public class LoginController {
     public String adminPage() {
         return "adminPage";
     }
+
+    @GetMapping("/session/user")
+    public ResponseEntity<?> getSessionUser(HttpSession session) {
+        SessionUser sessionUser = (SessionUser) session.getAttribute("user");
+        if (sessionUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(sessionUser);
+    }
+
 }
