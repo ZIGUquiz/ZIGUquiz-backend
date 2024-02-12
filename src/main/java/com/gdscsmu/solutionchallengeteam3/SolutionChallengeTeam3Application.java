76074867bde142addef4b132c788dc942dfa59ac
Sim-GyuBin <skb0606@gmail.com>
2024-02-12 17:55:30 +0900

    Feat: 시큐리티 로그인 창 안뜨게

--- src/main/java/com/gdscsmu/solutionchallengeteam3/SolutionChallengeTeam3Application.java
+++ src/main/java/com/gdscsmu/solutionchallengeteam3/SolutionChallengeTeam3Application.java
@@ -1,13 +1,11 @@
 package com.gdscsmu.solutionchallengeteam3;
 
-import com.gdscsmu.solutionchallengeteam3.config.auth.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SolutionChallengeTeam3Application {
 
 	public static void main(String[] args) {
