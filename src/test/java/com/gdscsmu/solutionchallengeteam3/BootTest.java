87dc06a020fa90225367138892fcd0b9ee00fd25
Sim-GyuBin <skb0606@gmail.com>
2024-02-15 00:16:21 +0900

    Test: 나라 랭킹 반환 테스트

--- src/test/java/com/gdscsmu/solutionchallengeteam3/BootTest.java
+++ src/test/java/com/gdscsmu/solutionchallengeteam3/BootTest.java
@@ -2,8 +2,11 @@ package com.gdscsmu.solutionchallengeteam3;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gdscsmu.solutionchallengeteam3.domain.Country;
 import com.gdscsmu.solutionchallengeteam3.domain.dto.UserDto;
 import com.gdscsmu.solutionchallengeteam3.domain.user.User;
+import com.gdscsmu.solutionchallengeteam3.repository.CountryRepository;
+import com.gdscsmu.solutionchallengeteam3.repository.QuizResultRepository;
 import com.gdscsmu.solutionchallengeteam3.repository.UserRepository;
 import com.gdscsmu.solutionchallengeteam3.service.CountryService;
 import com.gdscsmu.solutionchallengeteam3.util.JwtParser;
@@ -12,15 +15,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Base64;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @SpringBootTest
 public class BootTest {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CountryRepository countryRepository;
+    @Autowired
+    private QuizResultRepository quizResultRepository;
 
     String data = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImVkODA2ZjE4NDJiNTg4MDU0YjE4YjY2OWRkMWEwOWE0ZjM2N2FmYzQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3NTY2NDY4NDM2NDktZ2c5bjliN2Z2dHR0azdwZzZzZDIwbWNyaWFkbW4yYnAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3NTY2NDY4NDM2NDktZ2c5bjliN2Z2dHR0azdwZzZzZDIwbWNyaWFkbW4yYnAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDUwMzIxOTA1MjY1MzU0MTQ2ODYiLCJlbWFpbCI6Inl1bmJpbjA5MjVAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5iZiI6MTcwNzY1ODI4MSwibmFtZSI6IuyhsOycpOu5iCIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NJMVpObTZmbHJqNHN4ajlJYm81bEtPeUpXbVIxblFyc1F1MEd0eUo0U25mZz1zOTYtYyIsImdpdmVuX25hbWUiOiLsnKTruYgiLCJmYW1pbHlfbmFtZSI6IuyhsCIsImxvY2FsZSI6ImtvIiwiaWF0IjoxNzA3NjU4NTgxLCJleHAiOjE3MDc2NjIxODEsImp0aSI6IjI1ZDQ5Y2Y5OGE2ZDFlOWZmZWM3MTY4ZmU1OTM3MTkyN2MxZDM3MTIifQ.bXaEGYT92ocZwKjpcmuwwAbPUDXkUU4cWOJDqKTrg3pn5DOa4cmWhG_JeqrLl5y2FAEWi_3sCAulbIh3YUeSm5wqB6nQ7jKIOzcwNmVLtZqELjc7JHYcR7wbsOJJLsM0jUESFhN7fwTII4k28n4VL5w9QFFlP0UgSh8PgsE-0yNHSa9UNGTVgWGd0idPzWcoHULOOCtXlWlAEFDohW3oUZxhiGveUGfWFztfZJgU4Rox-oPKJeVBSPREQXpXmU-WSXlfTMthU-pyrtUkyd67hmR0LH5Ts06BwRcWzM8N0_Cw_ycO9IPQkWIMXdWCheMpllXEjMjO5YQ_8drCf_kx2A";
 
@@ -78,5 +84,31 @@ public class BootTest {
         userRepository.save(user);
 
         System.out.printf("User: %s", user);
+    }
+
+    @Test
+    public void 나라랭킹() {
+        //set
+        String[] name = {"aa", "bb", "cc","dd","ee","ff","gg","hh","ii","jj"};
+        Integer[] num = {1,4,25,6,9,5,12,6,7,8};
+        for (int i = 0; i < 10; i++) {
+            Country country = new Country(name[i], num[i]);
+            countryRepository.save(country);
+        }
+        //test
+        List<Country> countries = countryRepository.findAll();
+        countries.sort(Comparator.comparingInt(Country::getCount).reversed());
+
+        //자르기
+        int rankNum = 3;
+        if(countries.size() < 3)
+            rankNum = countries.size();
+
+        countries = countries.subList(0,rankNum);
+
+        Map<String, Integer> map = countries.stream().collect(
+                Collectors.toMap(Country::getCountryName, Country::getCount));
+
+        System.out.println(map);
     }
 }
