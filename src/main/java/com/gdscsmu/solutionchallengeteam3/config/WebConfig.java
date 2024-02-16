b431b3bc8dd0ab1e89b918074d7b603f1c17beb6
Sim-GyuBin <skb0606@gmail.com>
2024-02-16 16:34:40 +0900

    Feat: webconfig cors해결문 추가

--- src/main/java/com/gdscsmu/solutionchallengeteam3/config/WebConfig.java
+++ src/main/java/com/gdscsmu/solutionchallengeteam3/config/WebConfig.java
@@ -1,14 +1,24 @@
 package com.gdscsmu.solutionchallengeteam3.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@RequiredArgsConstructor
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET");
-//    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
+    }
+
 }
